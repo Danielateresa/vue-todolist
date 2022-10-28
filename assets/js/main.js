9a9bf7bfddef0c029049391c9ddf12ac9078db64
Danielateresa <dany.t_87@hotmail.it>
2022-10-28 15:12:43 +0200

    Printed tasks

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,14 +10,12 @@ Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul p
 Bonus:
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa) */
-
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
     data() {
-
         return {
-            message: 'Hello Vue!',
+
             tasks: [
                 {
                     text: 'svolgere esercizio',
@@ -43,3 +41,4 @@ createApp({
         }
     }
 }).mount('#app')
+
